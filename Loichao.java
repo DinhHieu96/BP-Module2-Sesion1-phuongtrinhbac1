dbf67e5ea0d0c801724b125d0656bfe057e50517
DinhHieu96 <truongdinhhieu1496@gamil.com>
2019-08-01 10:09:06 +0700

    hello

---
+++ Loichao.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Loichao {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter name: ");
+        String name=scanner.nextLine();
+        System.out.println("Hello "+name);
+    }
+}
